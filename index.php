6883550e795c409d66034f13e0f1c592f6292c1f
jorditosa <73916178+jorditosa@users.noreply.github.com>
2022-01-27 00:21:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="copyright" content="Redwood Team TV">
+  <meta name="audience" content="all">
+  <meta name="description" content="We are a production company with headquarters in Barcelona. We plan and produce creative, high-quality content based on mutual understanding with our clients.">
+  <meta name="keywords" content="Redwood, productora, publicidad">
+  <meta name="robots" content="index, all, follow">
+  <meta name="category" content="Film">
+  <meta name="telephone" content="+34 681 32 74 20">
+
+  <link rel="icon" type="image/x-icon" href="build/img/redwood_favicon.png">
+  <link rel="stylesheet" href="build/css/app.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+  <title>Redwood Team TV - Productora Audiovisual Barcelona</title>
+</head class="border-bottom border-primary border-2">
+
+<body class="d-flex vh-100 text-white">
+
+  <section class="container-fluid d-flex w-100 h-100 mx-0 p-0 flex-column justify-content-between">
+
+    <header class="pb-2">
+      <nav class="navbar navbar-expand-lg navbar-dark header-background pt-0">
+        <div class="container-fluid bg-header px-0 px-md-5">
+          <a class="navbar-brand text-start" href="index.php">
+            <img class="logo" src="build/img/redwood-logotext.webp" alt="logo redwood text">
+          </a>
+          <button class="navbar-toggler text-light fs-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse mb-4 mb-md-0" id="navbarNavAltMarkup">
+            <div class="navbar-nav w-100 d-flex flex-column flex-lg-row justify-content-end py-2">
+              <a class="nav-link fs-6 text-center px-md-4" href="works.php">Works</a>
+              <a class="nav-link fs-6 text-center px-md-4" href="#">Red...what?</a>
+              <a class="nav-link fs-6 text-center px-md-4" href="#">Team</a>
+              <a class="nav-link fs-6 text-center px-md-4" href="#">Contact</a>
+              <div class="languages d-flex justify-content-center">
+                <a class="nav-link fs-6 text-center d-md-none pe-2" href="">Esp</a>
+                <a class="nav-link fs-6 text-center d-md-none ps-2" href="">Cat</a>
+              </div>
+              <div class="footer-social text-center d-flex justify-content-center px-md-5 py-2 fs-6">
+                <i class="bi bi-vimeo pe-3"></i>
+                <i class="bi bi-linkedin"></i>
+                <i class="bi bi-instagram ps-3"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <div class='console-container d-none d-md-flex'>
+      <span id='text'></span>
+      <div class='console-underscore' id='console'>&#95;</div>
+    </div>
+
+
+    <!-- trabajo de botones 
+        <main class="container-fluid">
+          <div class="row justify-content-around menu-container">
+            <div class="menu col-7 col-md-3 mb-5 m-md-0">
+              <h2 class="menu-item fw-bold ">Works</h2>
+            </div>
+            <div class="menu col-7 col-md-3 my-4 m-md-0">
+              <h2 class="menu-item fw-bold">Reels</h2>
+            </div>
+            <div class="menu col-7 col-md-3 mt-5 m-md-0">
+              <h2 class="menu-item fw-bold">Service</h2>
+            </div>
+          </div>  
+        </main>
+        -->
+
+    <footer class="footer text-light text-center fs-6 py-2 px-5 d-flex flex-column flex-md-row justify-content-between bg-footer fixed-bottom">
+      <p class="py-2 fw-bold ms-md-5 my-auto">Redwood Team TV&copy; <span class="d-block d-md-inline">All rights reserved 2022.</span></p>
+      <div class="languages d-flex justify-content-center me-md-4 fw-bold">
+        <a class="nav-link text-light m-0 d-none d-md-flex" href="">Esp</a>
+        <a class="nav-link text-light m-0 d-none d-md-flex" href="">Cat</a>
+      </div>
+    </footer>
+  </section>
+
+  <!-- PLUGINS -->
+
+  <div class="cookie-container">
+    <p>
+      This website uses cookies. We use cookies to ensure that we give you the best experience on our
+      website to personalise content and adverts and to analyse our traffic using Google Analytics. To find out more, read our <a href="privpolicy.html">privacy policy</a> and our <a href="cookiepolicy.html">cookie policy</a>.
+    </p>
+    <button class="cookie-btn">Okay</button>
+  </div>
+
+  <section class="video">
+    <video src="build/img/intro_redwood.mp4" muted autoplay loop></video>
+  </section>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="build/js/app.js"></script>
+  <script src="build/js/hello.js"></script>
+
+
+</body>
+
+</html>
